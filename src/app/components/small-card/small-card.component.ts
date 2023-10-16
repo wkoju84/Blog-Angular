d0c60af04a35072f51e75b7141ef94e704c498b0
wkoju84 <wkoju84@gmail.com>
2023-10-16 14:07:59 -0300

    content created

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -11,6 +11,8 @@ export class SmallCardComponent {
   photoCover:string = ""
   @Input()
   cardTitle:string = ""
+  @Input()
+  Id:string = "0"
 
   constructor(){}
 
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+//Essa camada poderia ser um repository sendo chamado o BD
+
+export const dataFake = [{
+    "id": 1,
+    "title": "saiu um novo filme",
+    "description": "ola",
+    "photoCover": "https://multiversonoticias.com.br/wp-content/uploads/2023/09/herois-mais-fortes-dc-comics-4.jpg"
+},
+{
+    "id": 2,
+    "title": "saiu um novo filme",
+    "description": "ola",
+    "photoCover": "https://multiversonoticias.com.br/wp-content/uploads/2023/09/herois-mais-fortes-dc-comics-4.jpg"
+}
+]
